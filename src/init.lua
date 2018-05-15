3cd963f068ca4a655ab7d73a3c740b24e9467182
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-05-15 19:14:26 -0400

    document optional

--- src/init.lua
+++ src/init.lua
@@ -21,8 +21,13 @@ for _, typeName in pairs(BUILTIN_TYPE_NAMES) do
 	end
 end
 
+--[[
+	Returns a new validator function that behaves identically to the original,
+	but allows `nil` to be passed through.
+]]
 function PropTypes.optional(inner)
 	return function(value)
+		-- Specifically check for nil to avoid cases where "false" is not allowed
 		if value == nil then
 			return true
 		else
